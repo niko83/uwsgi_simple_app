12f6479fd7608eaaf5795e3afbc6f572363203d2
niko83 <ktotutus@gmail.com>
2012-06-20 18:28:59 -0400

    add view stack

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,10 @@ import sys
 import traceback                                            ## other uwsgi signals you define
 
 import uwsgidecorators
-
+import datetime, time
 
 def application(environ, start_response):
+    time.sleep(1)
     status = '200 OK'
     output = 'Hello World!'
     response_headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(output)))]
@@ -13,60 +14,60 @@ def application(environ, start_response):
     return [output]
 
 
-import datetime, time
 @uwsgidecorators.cron(30, 3, -1, -1, -1)
 def cron(num):
-    print '%s Every night on 3:00 run this code (signum %d)' % (datetime.datetime.now().strftime('%H:%m:%S'), num)
+    print '%s Every night on 3:00 run this code (signum %d)' % (datetime.datetime.now().strftime('%H:%M:%S'), num)
     time.sleep(50)
 
 @uwsgidecorators.timer(30)
+@uwsgidecorators.thread
 def f_timer(num):
-    print '%s Every 30 seconds run this code (signum %d)' % (datetime.datetime.now().strftime('%H:%m:%S'), num)
+    print '%s Every 30 seconds run this code (signum %d)' % (datetime.datetime.now().strftime('%H:%M:%S'), num)
     time.sleep(25)
 
 @uwsgidecorators.filemon('/var/www/uwsgi_simple_app/src/tmp/touchfiles/touch_me')
 def touch(num):
-    print '%s Touch file /var/www/uwsgi_simple_app/src/tmp/touchfiles/touch_me (signum %d)' % (datetime.datetime.now().strftime('%H:%m:%S'), num)
+    print '%s Touch file /var/www/uwsgi_simple_app/src/tmp/touchfiles/touch_me (signum %d)' % (datetime.datetime.now().strftime('%H:%M:%S'), num)
     #f3.spool()
 
-@uwsgidecorators.spool
-def f3(num):
-    FILE = '/var/www/env/mws/f1'
-    import time, random
-    f = open(FILE, "a")
-    numberS = random.randint(100, 900)
-    tab = random.randint(1, 20)
-    f.write('\n' + '+' * tab +'sta: '+str(numberS))
-    f.close()
+# @uwsgidecorators.spool
+# def f3(num):
+    # FILE = '/var/www/env/mws/f1'
+    # import time, random
+    # f = open(FILE, "a")
+    # numberS = random.randint(100, 900)
+    # tab = random.randint(1, 20)
+    # f.write('\n' + '+' * tab +'sta: '+str(numberS))
+    # f.close()
 
-    for a in xrange(120000000):
-        pass
+    # for a in xrange(120000000):
+        # pass
 
-    f = open(FILE, "a")
-    f.write('\n' + '+' * tab + 'end: '+str(numberS))
-    f.close()
-
-
-@uwsgidecorators.spool
-def f6(num):
-    FILE = '/var/www/env/mws/f1'
-    import time, random
-    f = open(FILE, "a")
-    numberS = random.randint(100, 900)
-    tab = random.randint(1, 20)
-    f.write('\n' + '+' * tab +'2ta: '+str(numberS))
-    f.close()
+    # f = open(FILE, "a")
+    # f.write('\n' + '+' * tab + 'end: '+str(numberS))
+    # f.close()
 
 
-    for a in xrange(120000000):
-        pass
+# @uwsgidecorators.spool
+# def f6(num):
+    # FILE = '/var/www/env/mws/f1'
+    # import time, random
+    # f = open(FILE, "a")
+    # numberS = random.randint(100, 900)
+    # tab = random.randint(1, 20)
+    # f.write('\n' + '+' * tab +'2ta: '+str(numberS))
+    # f.close()
 
-    f = open(FILE, "a")
-    f.write('\n' + '+' * tab + '2nd: '+str(numberS))
-    f.close()
-@uwsgidecorators.filemon('/var/www/touchfiles/f2')
-def f5(num):
-    f6.spool()
+
+    # for a in xrange(120000000):
+        # pass
+
+    # f = open(FILE, "a")
+    # f.write('\n' + '+' * tab + '2nd: '+str(numberS))
+    # f.close()
+# @uwsgidecorators.filemon('/var/www/touchfiles/f2')
+# def f5(num):
+    # f6.spool()
 
 # @uwsgidecorators.timer(1)
 # @uwsgidecorators.thread
@@ -89,33 +90,26 @@ def f5(num):
 # uwsgidecorators.filemon('/var/www/touchfiles/reload')(uwsgi.reload)
 
 
-stack_dump_file = '/var/www/touchfiles/stack'  ## given as an example; put this somewhere better than /tmp
-uwsgi_signal_number = 17                    ## nothing magic about this number; just can't conflict with
-try:
-    import uwsgi
-    import threading
+stack_dump_file = '/var/www/uwsgi_simple_app/src/tmp/touchfiles/stack'  ## given as an example; put this somewhere better than /tmp
+uwsgi_signal_number = uwsgidecorators.get_free_signal()
+import uwsgi
+import threading
 
-    if not os.path.exists(stack_dump_file):
-        open(stack_dump_file, 'w')
+#@uwsgidecorators.filemon('/var/www/uwsgi_simple_app/src/tmp/touchfiles/stack')
+def thread_dump(dummy_signum):
+    output = []
+    output.append("\n\n### BEGIN THREAD DUMP")
 
-    def thread_dump(dummy_signum):
-        output = []
-        output.append("\n\n### BEGIN THREAD DUMP")
+    thread_names = dict([(t.ident, t.name) for t in threading.enumerate()])
+    for threadId, stack in sys._current_frames().items():
+        output.append("# ThreadID: %s (%s)" % (threadId, thread_names.get(threadId, 'NO_NAME_AVAILABLE')))
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            output.append('File: "%s", line %d, in %s' % (filename, lineno, name))
+            if line:
+                output.append(" %s" % (line.strip()))
+    output.append("### END THREAD DUMP")
+    print '\n'.join(output)
 
-        thread_names = dict([(t.ident, t.name) for t in threading.enumerate()])
-        for threadId, stack in sys._current_frames().items():
-            output.append("# ThreadID: %s (%s)" % (threadId, thread_names.get(threadId, 'NO_NAME_AVAILABLE')))
-            for filename, lineno, name, line in traceback.extract_stack(stack):
-                output.append('File: "%s", line %d, in %s' % (filename, lineno, name))
-                if line:
-                    output.append(" %s" % (line.strip()))
-        output.append("### END THREAD DUMP")
-        print '\n'.join(output)
+uwsgi.register_signal(uwsgi_signal_number, 'workers', thread_dump)
+uwsgi.add_file_monitor(uwsgi_signal_number, stack_dump_file)
 
-    uwsgi.register_signal(uwsgi_signal_number, 'workers', thread_dump)
-    uwsgi.add_file_monitor(uwsgi_signal_number, stack_dump_file)
-
-except ImportError:
-    print >>sys.stderr, "Not running under uwsgi; unable to configure stack dump trigger"
-except IOError:
-    print >>sys.stderr, "IOError creating stack dump trigger %r" % (stack_dump_file,)
