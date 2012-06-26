406e1ea5cd1a8d80a97ec87e76a57bff0d79358d
niko83 <ktotutus@gmail.com>
2012-06-26 04:34:01 -0400

    continues implemeted

--- services.py
+++ services.py
@@ -4,8 +4,16 @@ import traceback                ## other uwsgi signals you define
 import datetime, time
 import uwsgidecorators
 import random
+import uwsgi
+import threading
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+LOGS_PATH = ROOT_PATH + '/tmp/logs/'
+TOUCHFILES_PATH = ROOT_PATH + '/tmp/touchfiles/'
+
+def _colored(text, color='reset'):
+    COLORS = { 'reset' : "\x1b[0m", 'green':"\x1b[32;01m", 'red':"\x1b[31;01m", 'gray':"\x1b[90;01m", 'white':"\x1b[1;01m"}
+    return ''.join([COLORS[color], text, COLORS['reset']])
 
 @uwsgidecorators.cron(30, 3, -1, -1, -1)
 def cron(num):
@@ -18,57 +26,49 @@ def f_timer(num):
     print '%s Every 30 seconds run this code (signum %d)' % (datetime.datetime.now().strftime('%H:%M:%S'), num)
     time.sleep(25)
 
-@uwsgidecorators.filemon(ROOT_PATH + '/tmp/touchfiles/touch_me')
+@uwsgidecorators.filemon(TOUCHFILES_PATH + 'touch_me')
 def touch(num):
-    print '%s Touch file %s (signum %d)' % (datetime.datetime.now().strftime('%H:%M:%S'), ROOT_PATH + '/tmp/touchfiles/touch_me', num)
+    print '%s Touch file %s (signum %d)' % (datetime.datetime.now().strftime('%H:%M:%S'), TOUCHFILES_PATH + 'touch_me', num)
     spool_proc.spool()
 
-@uwsgidecorators.filemon(ROOT_PATH + '/tmp/touchfiles/start_spool_proc')
+@uwsgidecorators.filemon(TOUCHFILES_PATH + 'start_spool_proc')
 def start_spool(num):
     print '%s add procces  to spool (signum %d)' % (datetime.datetime.now().strftime('%H:%M:%S'), num)
     spool_proc.spool()
 
 @uwsgidecorators.spool
 def spool_proc(num):
-    log_file = ROOT_PATH + '/tmp/logs/spool.log'
-    f = open(log_file, "a")
-    numberS = random.randint(100, 900)
+    log_file = LOGS_PATH + 'spool.log'
+    unique_id = random.randint(100, 900)
     tab = random.randint(1, 20)
-    f.write('\n' + '+' * tab +'start: '+str(numberS))
-    f.close()
-    time.sleep(60)
+
     f = open(log_file, "a")
-    f.write('\n' + '+' * tab + 'end  : '+str(numberS))
+    f.write('\n' + '+' * tab +'start: '+str(unique_id))
     f.close()
 
-# @uwsgidecorators.spool
-# def f6(num):
-    # FILE = '/var/www/env/mws/f1'
-    # import time, random
-    # f = open(FILE, "a")
-    # numberS = random.randint(100, 900)
-    # tab = random.randint(1, 20)
-    # f.write('\n' + '+' * tab +'2ta: '+str(numberS))
-    # f.close()
+    time.sleep(60)
 
+    f = open(log_file, "a")
+    f.write('\n' + '+' * tab + 'end  : '+str(unique_id))
+    f.close()
 
-    # for a in xrange(120000000):
-        # pass
+@uwsgidecorators.filemon(TOUCHFILES_PATH + 'stack', target='workers')
+def thread_dump(signum):
+    output = ["\n\n"]
+    output.append('{:=^60s} {:s}'.format(' BEGIN THREAD DUMP ', datetime.datetime.now().strftime('%H:%M:%S')))
 
-    # f = open(FILE, "a")
-    # f.write('\n' + '+' * tab + '2nd: '+str(numberS))
-    # f.close()
-# @uwsgidecorators.filemon('/var/www/touchfiles/f2')
-# def f5(num):
-    # f6.spool()
+    thread_names = dict([(t.ident, t.name) for t in threading.enumerate()])
+    for threadId, stack in sys._current_frames().items():
+        output.append(_colored('{:s}ThreadID: {:d} ({:s})'.format(' '*4, threadId, thread_names.get(threadId, 'NO_NAME_AVAILABLE')), 'white'))
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            output.append(_colored('{:s}File: "{:s}", line {:d}, in {:s}'.format(' '*8, filename, lineno, name),'gray'))
+    output.append('{:=^60s}'.format(' END THREAD DUMP '))
+    print '\n'.join(output)
 
-# @uwsgidecorators.timer(1)
-# @uwsgidecorators.thread
-# def f4(num):
-    # import time
-    # print 'start'
-    # time.sleep(50)
-    # print 'finish'
+@uwsgidecorators.postfork
+def a_running_thread_with_args():
+    print 'postfork job is started'
+
 
 #@uwsgidecorators.filemon('/var/www/touchfiles/f1')
 #@uwsgidecorators.postfork
@@ -78,31 +78,4 @@ def spool_proc(num):
     # while True:
         # time.sleep(2)
         # print("Hello %s (from arged-thread)" % who)
-
-# import uwsgi
-# uwsgidecorators.filemon('/var/www/touchfiles/reload')(uwsgi.reload)
-
-
-stack_dump_file = ROOT_PATH + '/tmp/touchfiles/stack'  ## given as an example; put this somewhere better than /tmp
-uwsgi_signal_number = uwsgidecorators.get_free_signal()
-import uwsgi
-import threading
-
-#@uwsgidecorators.filemon('/var/www/uwsgi_simple_app/src/tmp/touchfiles/stack')
-def thread_dump(dummy_signum):
-    output = []
-    output.append("\n\n### BEGIN THREAD DUMP")
-
-    thread_names = dict([(t.ident, t.name) for t in threading.enumerate()])
-    for threadId, stack in sys._current_frames().items():
-        output.append("# ThreadID: %s (%s)" % (threadId, thread_names.get(threadId, 'NO_NAME_AVAILABLE')))
-        for filename, lineno, name, line in traceback.extract_stack(stack):
-            output.append('File: "%s", line %d, in %s' % (filename, lineno, name))
-            if line:
-                output.append(" %s" % (line.strip()))
-    output.append("### END THREAD DUMP")
-    print '\n'.join(output)
-
-uwsgi.register_signal(uwsgi_signal_number, 'workers', thread_dump)
-uwsgi.add_file_monitor(uwsgi_signal_number, stack_dump_file)
 
